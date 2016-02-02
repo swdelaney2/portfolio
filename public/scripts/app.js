1f42e41d54dcaf5b6fe9974d777c5695a46c6e29
Stephen Delaney <swdelaney2@gmail.com>
2016-02-02 14:51:04 -0600

    initial js and css set up

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,43 @@
-// console.log('app. js loaded');
 // http://stackoverflow.com/questions/7717527/jquery-smooth-scrolling-when-clicking-an-anchor-link
 $(document).ready(function(){
 
-$('a').click(function(){
-  // console.log('link clicked');
+$('.anchor').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 500);
     return false;
 });
+
+
+// http://stackoverflow.com/questions/1328723/how-to-generate-a-simple-popup-using-jquery
+
+
+function deselect(e) {
+  $('.pop').slideFadeToggle(function() {
+    e.removeClass('selected');
+  });
+}
+
+$(function() {
+  $('#contact').on('click', function() {
+    if($(this).hasClass('selected')) {
+      deselect($(this));
+    } else {
+      $(this).addClass('selected');
+      $('.pop').slideFadeToggle();
+    }
+    return false;
+  });
+
+  $('.close').on('click', function() {
+    deselect($('#contact'));
+    return false;
+  });
 });
+
+$.fn.slideFadeToggle = function(easing, callback) {
+  return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
+};
+
+
+}); // end of onload
